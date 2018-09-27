4929a95a8aaf922bd3eb78a70bfdb9cad451ea2e
Meng-m <826110738@qq.com>
2018-09-27 13:34:47 +0800

    Site updated: 2018-09-27 13:34:28

---
+++ js/src/custom.js
@@ -0,0 +1,10 @@
+$(function () {
+  $.backstretch([
+    "/image/bg3.jpg",
+    "/image/snow-1.jpg",
+    "/image/snow-2.jpg",
+    "/image/snow-3.jpg",
+    "/image/snow-4.jpg",
+    "/image/snow-5.jpg",
+  ], { duration: 5000, fade: 1500 });
+});
